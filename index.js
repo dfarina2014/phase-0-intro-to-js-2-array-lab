016b206238826f73c07b11043a9e40c3f358c402
daren farina <daren.farina@gmail.com>
2022-08-18 13:03:39 -0400

    commit tree fb42e516682c6ac5f01f7d2f9ee26645e8a31bc9

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendCat(name){
+   cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+  }
+
+  function prependCat(name){
+    const newCats2 = [...cats];
+    newCats2.unshift(name);
+    return newCats2;
+  }
+
+  function removeLastCat(){
+    const newCats3 = cats.slice();
+    newCats3.pop();
+    return newCats3;
+  }
+
+  function removeFirstCat(){
+    const newCats4 = cats.slice();
+    newCats4.shift();
+    return newCats4;
+  }
